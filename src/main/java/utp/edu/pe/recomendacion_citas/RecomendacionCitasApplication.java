1aa7a379a8a199db39ebe1c85fa60b8cee84baca
Oxand <andre_vra@hotmail.com>
2026-09-07 16:33:32 -0500

    C1:Estructura principal del proyecto

---
+++ src/main/java/utp/edu/pe/recomendacion_citas/RecomendacionCitasApplication.java
@@ -0,0 +1,13 @@
+package utp.edu.pe.recomendacion_citas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendacionCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendacionCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utp/edu/pe/recomendacion_citas/RecomendacionCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.edu.pe.recomendacion_citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendacionCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
