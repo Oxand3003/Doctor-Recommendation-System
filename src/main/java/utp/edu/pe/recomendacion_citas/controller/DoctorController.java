d3072e7afa8bb871219c49fc03172731b3286416
ARamirezA <u23206491@utp.edu.pe>
2026-09-11 11:38:58 -0500

    feat: agregar dependencia springdoc

--- src/main/java/utp/edu/pe/recomendacion_citas/controller/DoctorController.java
+++ src/main/java/utp/edu/pe/recomendacion_citas/controller/DoctorController.java
@@ -1,5 +1,15 @@
 package utp.edu.pe.recomendacion_citas.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController 
+@RequestMapping("/api/v1/doctors")
+@Tag(name = "Doctores", description = "Consulta y recomendación de médicos")
 public class DoctorController {
 
 }
