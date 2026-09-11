ccc1327fc99d76db1d883d3498a9d0e0cd663d6f
ERosasR <U233306936@utp.edu.pe>
2026-09-11 16:18:14 -0500

    chore: configurar base de datos de desarrollo y datos de ejemplo

--- src/test/java/utp/edu/pe/recomendacion_citas/RecomendacionCitasApplicationTests.java
+++ src/test/java/utp/edu/pe/recomendacion_citas/RecomendacionCitasApplicationTests.java
@@ -2,8 +2,9 @@ package utp.edu.pe.recomendacion_citas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import utp.edu.pe.recomendaciones.RecomendacionCitasApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = RecomendacionCitasApplication.class)
 class RecomendacionCitasApplicationTests {
 
 	@Test
