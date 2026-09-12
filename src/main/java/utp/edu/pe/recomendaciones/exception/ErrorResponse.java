f5e0803cf0412a31eb4e8bfc30b0107e5659a7c7
JAlvarezF <u23233957@utp.edu.pe>
2026-09-11 23:01:11 -0500

    feat: agregar respuesta de error para Usuarios

---
+++ src/main/java/utp/edu/pe/recomendaciones/exception/ErrorResponse.java
@@ -0,0 +1,70 @@
+package pe.edu.salud.recomendaciones.exception;
+
+
+
+import java.time.LocalDateTime;
+
+/**
+ * Cuerpo estructurado que 
+ * se devuelve al cliente ante 
+ * cualquier error.
+ */
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String mensaje;
+    private String ruta;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(int status, String error, String mensaje, String ruta) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.mensaje = mensaje;
+        this.ruta = ruta;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+}
